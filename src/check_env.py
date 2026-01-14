9ec2948a0bb3dfb83c9af3cd03b3e4610ffafd7f
Anusha Khadka <aanukhadka4@gmail.com>
2026-01-13 23:47:19 -0500

    Milestone 1: setup venv, dependencies, and env check script

---
+++ src/check_env.py
@@ -0,0 +1,27 @@
+﻿import sys
+
+def main():
+    print("Python executable:")
+    print(sys.executable)
+    print()
+
+    print("Checking installed packages...")
+
+    import fastapi
+    import uvicorn
+    import pandas
+    import numpy
+    import pydicom
+    import PIL
+
+    print("fastapi:", fastapi.__version__)
+    print("uvicorn:", uvicorn.__version__)
+    print("pandas:", pandas.__version__)
+    print("numpy:", numpy.__version__)
+    print("pydicom:", pydicom.__version__)
+    print("pillow:", PIL.__version__)
+
+    print("\n✅ Environment check PASSED!")
+
+if __name__ == "__main__":
+    main()
